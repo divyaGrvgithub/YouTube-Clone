7de01108e87f83653adcf660bc5f185fa90d15d2
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-09-01 22:55:11 +0530

    Update ShimmerWatch.js

--- src/ShimmerUI/ShimmerWatch.js
+++ src/ShimmerUI/ShimmerWatch.js
@@ -9,7 +9,7 @@ const ShimmerWatch = () => {
         height="500"
         src=""
         title="YouTube video player"
-        // frameBorder="0"
+        // frameBorder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
