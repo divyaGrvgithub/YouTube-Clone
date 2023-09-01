ad742062189deccad166dae434465c8a5a8599c9
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-09-01 22:59:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( 
   <React.StrictMode>
     <App />
   </React.StrictMode>
