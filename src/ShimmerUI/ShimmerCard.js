24a3dd2ea92217cf72e192ee5d1acc6bb2d90399
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-09-01 22:54:58 +0530

    Update ShimmerCard.js

--- src/ShimmerUI/ShimmerCard.js
+++ src/ShimmerUI/ShimmerCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ShimmerCard = () => {
+const ShimmerCard = () => { 
   return (
       <div className="flex flex-wrap">
         <div className="bg-gray-200 p-2 m-3 h-48 w-72 rounded-xl">  
