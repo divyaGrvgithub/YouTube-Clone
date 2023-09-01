4cad47a2be9648d14a5ce0034ca248b4b80536ab
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-09-01 22:58:59 +0530

    Update store.js

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSlice from "./appSlice";
 import searchSlice from "./searchSlice";
-import chatSlice from "./chatSlice";
+import chatSlice from "./chatSlice"; 
 
 const store = configureStore({
   reducer: {
