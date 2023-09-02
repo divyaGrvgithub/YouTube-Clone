da6e9dd72768b1ae6c058d06cdf9bf1d38e50e98
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-09-02 16:03:28 +0530

    Update ShimmerCard.js

--- src/ShimmerUI/ShimmerCard.js
+++ src/ShimmerUI/ShimmerCard.js
@@ -4,7 +4,7 @@ const ShimmerCard = () => {
   return (
       <div className="flex flex-wrap">
         <div className="bg-gray-200 p-2 m-3 h-48 w-72 rounded-xl">  
-        </div> 
+        </div>    
         <div className="bg-gray-200 p-2 m-3 h-48 w-72 rounded-xl">  
         </div>   
         <div className="bg-gray-200 p-2 m-3 h-48 w-72 rounded-xl">  
