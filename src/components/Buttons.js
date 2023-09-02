9e28c2902b4a16d5115cd6235f27d951cea5bd05
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-09-02 12:09:56 +0530

    Update Buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,7 +5,7 @@ const Buttons = ({name}) => {
     <div> 
       <button className=' border border-gray-200 shadow-sm px-5 py-1 bg-gray-200 rounded-lg m-2 mt-5 hover:bg-black hover:text-white hover:ring-sky-500 '>{name}</button> 
     </div>
-  )
+  )                   
 }
 
 export default Buttons
