722446e78a038a8dcd3ed7243104255d5d4e2a0a
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-09-02 16:05:14 +0530

    Update ChatMessages.js

--- src/components/ChatMessages.js
+++ src/components/ChatMessages.js
@@ -8,7 +8,7 @@ const ChatMessages = ({name,message}) => {
       <img
           className="h-6"
           src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-          alt="user-logo"
+          alt="user-logo"  
         />
         <p className=' px-2 text-sm font-bold'>{name}</p>
         <p className=' text-sm' >{message}</p>
