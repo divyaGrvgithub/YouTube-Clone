c2e2ae7b6cbfa318d63bb3fc78c4ff2780d46440
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-09-02 12:09:36 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import { Outlet } from 'react-router-dom'
-
+  
 const Body = () => {
   return (
     <div className='app flex container'>
