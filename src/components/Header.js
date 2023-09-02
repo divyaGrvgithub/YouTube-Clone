b994a5bfcbf19091b5d39fcb3fad897ea5c06360
Divyamala Tiwari <119675338+divyaGrvgithub@users.noreply.github.com>
2023-09-02 12:11:00 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { BsBell } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
 import { togglemenu } from "../utils/appSlice";
 import { YOUTUBE_SEARCH_API } from "../contants/api_data"; 
-import { cacheResults } from "../utils/searchSlice";
+import { cacheResults } from "../utils/searchSlice";   
 // import DarkMode from "../DarkMode/DarkMode";
 
 const Header = () => {
